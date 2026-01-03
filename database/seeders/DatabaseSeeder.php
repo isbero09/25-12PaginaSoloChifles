f8ea17e59936771e8fc2d41e86eff40e6b072355
isbero09 <isme09mocha@gmail.com>
2026-01-03 13:05:51 -0500

    Add default admin user seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,11 +4,26 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Database\Seeders\PagesTableSeeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
     public function run()
     {
+        // Crear usuario administrador por defecto si no existe
+        User::firstOrCreate(
+            ['email' => 'admin@example.com'],
+            [
+                'name' => 'Administrador',
+                'password' => Hash::make('password123'),
+                'is_admin' => true,
+                'email_verified_at' => now(),
+                'remember_token' => Str::random(10),
+            ]
+        );
+
         $this->call(PagesTableSeeder::class);
     }
 }
